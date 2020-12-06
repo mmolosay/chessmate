7418bfe039f6395b21bd72028e104dc22d58c4b2
ordogod <mikhail.molosay@effective-soft.com>
2020-12-06 20:23:40 +0300

    Set up base DI

---
+++ app/src/main/java/com/ordolabs/chessmate/di/ViewModelModule.kt
@@ -0,0 +1,13 @@
+package com.ordolabs.chessmate.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+
+//    viewModel {
+        // ViewModel(
+        // // there will be usecases
+    // )
+//    }
+}

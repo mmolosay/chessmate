f15d42f71eb29de2c5249919ec9846111e748258
ordogod <mikhail.molosay@effective-soft.com>
2020-12-08 02:08:57 +0300

    Implement working stopwatch

--- app/src/main/java/com/ordolabs/chessmate/ui/fragment/HomeClockTabFragment.kt
+++ app/src/main/java/com/ordolabs/chessmate/ui/fragment/HomeClockTabFragment.kt
@@ -1,13 +1,25 @@
 package com.ordolabs.chessmate.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.ordolabs.chessmate.R
+import com.ordolabs.chessmate.viewmodel.HomeViewModel
+import kotlinx.android.synthetic.main.fragment_home_tab_clock.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeClockTabFragment private constructor() : Fragment() {
+
+    private val homeVM: HomeViewModel by viewModel()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        observeStopwatchTime()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +28,49 @@ class HomeClockTabFragment private constructor() : Fragment() {
     ): View? {
         val layout = R.layout.fragment_home_tab_clock
         return inflater.inflate(layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setStopwatch()
+        setResetStopwatchButton()
+        setStartStopButton()
+    }
+
+    private fun setStopwatch() {
+
+    }
+
+    private fun setResetStopwatchButton() {
+        tab_clock_btn_reset.setOnClickListener {
+            homeVM.resetStopwatch()
+        }
+    }
+
+    private fun setStartStopButton() {
+        tab_clock_btn_startstop.setOnClickListener {
+            val running = homeVM.isStopwatchRunning()
+            if (running) {
+                homeVM.stopStopwatch()
+            } else{
+                homeVM.startStopwatch()
+            }
+            alterStartStopButtonIcon(running)
+        }
+    }
+
+    private fun alterStartStopButtonIcon(setStart: Boolean) {
+        val iconRes = if (setStart)
+            R.drawable.ic_start_normal
+        else
+            R.drawable.ic_stop_normal
+        val icon = ContextCompat.getDrawable(requireContext(), iconRes)
+        tab_clock_btn_startstop.setImageDrawable(icon)
+    }
+
+    private fun observeStopwatchTime() {
+        homeVM.stopwatchTime.observe(this) { time ->
+            tab_clock_stopwatch.text = time
+        }
     }
 
     companion object {

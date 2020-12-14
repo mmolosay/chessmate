24a2a241f99f35d678418348d2f4866607673c21
ordogod <mikhail.molosay@effective-soft.com>
2020-12-14 18:09:17 +0300

    Move timer view and warn view to separate ConstraintLayout

---
+++ app/src/main/java/com/ordolabs/chessmate/ui/view/TimerConstraintLayout.kt
@@ -0,0 +1,12 @@
+package com.ordolabs.chessmate.ui.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class TimerConstraintLayout @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+
+}
